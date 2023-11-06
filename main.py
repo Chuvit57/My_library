7d24f7dc5e6bc4c73edfa4440a597545229ac750
Vitaly Chu <chuvitg@gmail.com>
2023-11-07 00:23:30 +0300

    Creating the first table

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import sqlite3 as sq
 with sq.connect("my_books.db") as con:
     cur = con.cursor()
 
-    cur.execute("""
-    """)
+    cur.execute("""CREATE TABLE IF NOT EXISTS books (
+    name_book TEXT,
+    description TEXT,
+    year INTEGER
+    )""")
 
 

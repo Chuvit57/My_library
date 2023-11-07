78ec5ad30ae4a87523097bd63146bf9384079474
Vitaly Chu <chuvitg@gmail.com>
2023-11-07 09:53:24 +0300

    I continue to create the datadase

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import sqlite3 as sq
 with sq.connect("my_books.db") as con:
     cur = con.cursor()
 
+    # cur.execute("DROP TABLE books")  # Удаляет таблицу
     cur.execute("""CREATE TABLE IF NOT EXISTS books (
-    name_book TEXT,
+    book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name_book TEXT NOT NULL,
     description TEXT,
     year INTEGER
     )""")

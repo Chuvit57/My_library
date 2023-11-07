9b1364c0ce7b7e5337829feef8a1a6af9a1ad6c2
Vitaly Chu <chuvitg@gmail.com>
2023-11-07 10:09:30 +0300

    Created the function'create_db'

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sqlite3 as sq
 
-with sq.connect("my_books.db") as con:
-    cur = con.cursor()
 
-    # cur.execute("DROP TABLE books")  # Удаляет таблицу
-    cur.execute("""CREATE TABLE IF NOT EXISTS books (
-    book_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name_book TEXT NOT NULL,
-    description TEXT,
-    year INTEGER
-    )""")
+def create_db():
+    with sq.connect("my_books.db") as con:
+        cur = con.cursor()
+
+        # cur.execute("DROP TABLE books")  # Удаляет таблицу
+        cur.execute("""CREATE TABLE IF NOT EXISTS books (
+        book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name_book TEXT NOT NULL,
+        description TEXT,
+        year INTEGER
+        )""")
 
 
+if __name__ == '__main__':
+    create_db()
